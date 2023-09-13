7329264afad2a6febb9b87822f860b5e42ffde9c
Balladares67 <juan.balladares01@gmail.com>
2023-09-13 14:23:52 -0300

    hoy_corregí_errores

--- entrega_practica1_codigo.R.R
+++ entrega_practica1_codigo.R.R
@@ -14,12 +14,21 @@ nn<- substr(l,6,7)
 matap<- 20-m
 
 #presentacion de datos
-linea<- paste("el alumno ",n," se inscribió como alumno de
-              Exactas en el puesto ",puesto," en el año 20",
-              nn," y debe aprobar ",matap," materias para
-              obtener el título de grado", sep="")
+linea<- paste("el alumno ",n," se inscribió como alumno de Exactas",
+" en el puesto ",puesto, " en el año 20",nn," y debe aprobar ",
+matap," materias para obtener el título de grado", sep="",
+collapse = "\n")
 print(linea)
 
 #comentarios
-#veo que queda separado el 2017(okk) y los saltos de linea los marca con \n
+#veo que queda separado el 2017(okk) y los saltos de linea los marca con \n(okk)
 #si pone mal algun dato, necesito de los ciclos para volver a ingresarlo
+
+#El ejercicio está desaprobado. El programa corre, pero no cumple con lo pedido.
+#El mensaje no se imprime correctamente. 
+#Comentarios para la reentrega el jueves 14/9: 
+#Presta atención  a cómo estás obteniendo la información de la libreta. 
+#Tenes varios espacios de más en el paste de la L17. (okk)
+#En el pseudo código decis que corroboras que la información es correcta, eso no
+#está en el código, o si? Revisalo(okk)
+#Rocio 
